8913b51030264de0cfb775ac1d41c913559cf2a0
Dinar <dinar-97@mail.ru>
2024-04-10 13:21:25 +0300

    added testRunner for qualit

---
+++ src/test/java/org/ibs/fazlyakhmetov/tests/qualit/RunnerTest.java
@@ -0,0 +1,19 @@
+package org.ibs.fazlyakhmetov.tests.qualit;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.ConfigurationParameters;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
+        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features/qualit.feature"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.ibs.fazlyakhmetov"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
+})
+public class RunnerTest {
+}
