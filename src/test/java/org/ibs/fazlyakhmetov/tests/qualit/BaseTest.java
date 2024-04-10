ed00748a0d9b050186f0c602c11ecb9fab999f1a
Dinar <dinar-97@mail.ru>
2024-04-10 16:30:52 +0300

    added screenshots after steps

--- src/test/java/org/ibs/fazlyakhmetov/tests/qualit/BaseTest.java
+++ src/test/java/org/ibs/fazlyakhmetov/tests/qualit/BaseTest.java
@@ -1,17 +1,20 @@
 package org.ibs.fazlyakhmetov.tests.qualit;
 
 import io.cucumber.java.After;
-import io.cucumber.java.AfterAll;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
-import io.cucumber.java.BeforeAll;
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
 import org.aeonbits.owner.ConfigFactory;
 import org.ibs.fazlyakhmetov.config.qualit.QualitConfig;
 import org.junit.jupiter.api.Tag;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.ByteArrayInputStream;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 @Tag("@all")
 public class BaseTest {
@@ -26,6 +29,12 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.get(configOwner.baseUrl());
+    }
+
+    @AfterStep
+    public void afterStep(Scenario scenario) throws IllegalMonitorStateException {
+        Allure.addAttachment("Screenshot step", new ByteArrayInputStream(((TakesScreenshot) driver).
+                getScreenshotAs(OutputType.BYTES)));
     }
 
     @After(value = "@all")
