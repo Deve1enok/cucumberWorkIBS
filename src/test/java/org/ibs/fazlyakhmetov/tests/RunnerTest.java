85f8f838bd8ad7a306ac1b39c490e41cd7aa825f
Dinar <dinar-97@mail.ru>
2024-04-11 19:24:49 +0300

    update  runner

--- src/test/java/org/ibs/fazlyakhmetov/tests/RunnerTest.java
+++ src/test/java/org/ibs/fazlyakhmetov/tests/RunnerTest.java
@@ -1,4 +1,4 @@
-package org.ibs.fazlyakhmetov.tests.qualit;
+package org.ibs.fazlyakhmetov.tests;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.ConfigurationParameters;
@@ -9,9 +9,9 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
-        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features/qualit.feature"),
+@ConfigurationParameters(value = {
+//        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all,@jdbc"),
+        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.ibs.fazlyakhmetov"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
 })
