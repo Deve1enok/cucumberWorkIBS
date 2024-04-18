b8a21de3dd19205552a4ed72be70b699a846525d
Dinar <dinar-97@mail.ru>
2024-04-18 14:34:32 +0300

    fix capabilities

--- src/test/java/org/ibs/fazlyakhmetov/tests/qualit/BaseTest.java
+++ src/test/java/org/ibs/fazlyakhmetov/tests/qualit/BaseTest.java
@@ -8,11 +8,12 @@ import io.qameta.allure.Allure;
 import org.aeonbits.owner.ConfigFactory;
 import org.ibs.fazlyakhmetov.config.qualit.QualitConfig;
 import org.junit.jupiter.api.Tag;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.ByteArrayInputStream;
@@ -31,19 +32,19 @@ public class BaseTest {
         String selenoidRun = System.getProperty("selenoid.run");
 
         if (selenoidRun != null && selenoidRun.equalsIgnoreCase("true")) {
-            System.setProperty(configOwner.remoteDriver(), "remote");
-            System.setProperty(configOwner.selenoidUrl(), "http://149.154.71.152:4444/wd/hub");
-            System.setProperty(configOwner.typeBrowser(), "chrome");
+//            System.setProperty(configOwner.remoteDriver(), "remote");
+//            System.setProperty(configOwner.selenoidUrl(), "http://149.154.71.152:4444/wd/hub");
+//            System.setProperty(configOwner.typeBrowser(), "chrome");
 
-            ChromeOptions options = new ChromeOptions();
-            options.setCapability("browserName", configOwner.typeBrowser());
-            options.setCapability("browserVersion", "109.0");
-            options.addArguments("start-maximized");
-            options.setCapability("selenoid:options", Map.<String, Object>of(
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("browserName", System.getProperty("type.browser"));
+            capabilities.setCapability("browserVersion", System.getProperty("version.browser"));
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                     "enableVNC", true,
                     "enableVideo", true
             ));
-            driver = new RemoteWebDriver(URI.create(configOwner.selenoidUrl()).toURL(), options);
+            driver = new RemoteWebDriver(URI.create(configOwner.selenoidUrl()).toURL(), capabilities);
+            driver.manage().window().setSize(new Dimension(1920, 1080));
             driver.get(configOwner.baseUrl());
         } else {
             System.setProperty("webdriver.chromedriver.driver", configOwner.chromeDriver());
@@ -53,6 +54,14 @@ public class BaseTest {
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
             driver.get(configOwner.baseUrl());
         }
+        //            ChromeOptions options = new ChromeOptions();
+//            options.setCapability("browserName", System.getProperty(configOwner.typeBrowser()));
+//            options.setCapability("browserVersion", "109.0");
+//            options.addArguments("start-maximized");
+//            options.setCapability("selenoid:options", Map.<String, Object>of(
+//                    "enableVNC", true,
+//                    "enableVideo", true
+//            ));
     }
 
     @AfterStep(value = "@all")
